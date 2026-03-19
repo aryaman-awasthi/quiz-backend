caf490973ceaf3983dfb5e60054db2725f661860
Aryaman <aryaman.awasthi79@gmail.com>
2026-03-19 23:13:45 +0530

    update: added response entity
    Added response entity and exception handelling for better output

--- src/main/java/com/test/quizapp/controller/QuestionController.java
+++ src/main/java/com/test/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.test.quizapp.controller;
 import com.test.quizapp.model.Question;
 import com.test.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,27 +16,27 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestion () {
+    public ResponseEntity<List<Question>> getAllQuestion () {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory (@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
     @DeleteMapping("delete/{id}")
-    public String deleteQuestion(@PathVariable int id) {
+    public ResponseEntity<String> deleteQuestion(@PathVariable int id) {
         return questionService.deleteQuestion(id);
     }
 
     @PutMapping("update/{id}")
-    public Question updateQuestion(@PathVariable int id, @RequestBody Question question) {
+    public ResponseEntity<Question> updateQuestion(@PathVariable int id, @RequestBody Question question) {
         return questionService.updateQuestion(id, question);
     }
 }
--- src/main/java/com/test/quizapp/service/QuestionService.java
+++ src/main/java/com/test/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.test.quizapp.service;
 import com.test.quizapp.model.Question;
 import com.test.quizapp.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,38 +16,66 @@ public class QuestionService {
     @Autowired
     QuestionRepository questionRepository;
 
-    public List<Question> getAllQuestions() {
-        return questionRepository.findAll();
-    }
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionRepository.findAll(), HttpStatus.OK);
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionRepository.findByCategory(category);
-    }
-
-
-    public String addQuestion(Question question) {
-        questionRepository.save(question);
-        return "success";
-    }
-
-    public String deleteQuestion(int id) {
-        questionRepository.deleteById(id);
-        return "success";
-    }
-
-    public Question updateQuestion(int id, Question question) {
-        Question existing = questionRepository.findById(id).orElse(null);
-        if (existing != null) {
-            existing.setCategory(question.getCategory());
-            existing.setDifficultylevel(question.getDifficultylevel());
-            existing.setQuestionTitle(question.getQuestionTitle());
-            existing.setOption1(question.getOption1());
-            existing.setOption2(question.getOption1());
-            existing.setOption3(question.getOption3());
-            existing.setOption4(question.getOption4());
-            existing.setRightAnswer(question.getRightAnswer());
-            return questionRepository.save(existing);
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
-        return null;
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NO_CONTENT);
+
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionRepository.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NO_CONTENT);
+    }
+
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try {
+            questionRepository.save(question);
+            return new ResponseEntity<>("success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return new ResponseEntity<>("cannot add question", HttpStatus.NOT_MODIFIED);
+
+    }
+
+    public ResponseEntity<String> deleteQuestion(int id) {
+        try {
+            questionRepository.deleteById(id);
+            return new ResponseEntity<>("success", HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return new ResponseEntity<>("cannot add question", HttpStatus.NOT_MODIFIED);
+    }
+
+    public ResponseEntity<Question> updateQuestion(int id, Question question) {
+        Question existing = questionRepository.findById(id).orElse(null);
+        try {
+            if (existing != null) {
+                existing.setCategory(question.getCategory());
+                existing.setDifficultylevel(question.getDifficultylevel());
+                existing.setQuestionTitle(question.getQuestionTitle());
+                existing.setOption1(question.getOption1());
+                existing.setOption2(question.getOption1());
+                existing.setOption3(question.getOption3());
+                existing.setOption4(question.getOption4());
+                existing.setRightAnswer(question.getRightAnswer());
+                return new ResponseEntity<>(questionRepository.save(existing), HttpStatus.OK);
+            }
+        } catch (Exception e) {
+             System.out.println(e.toString());
+        }
+        return new ResponseEntity<>(new Question(), HttpStatus.NO_CONTENT);
     }
 }
