fa92ceb40d8eef9c11abaa909560dcdcc156d11c
Aryaman <aryaman.awasthi79@gmail.com>
2026-03-19 20:30:04 +0530

    feat: add question endpoint
    created an endpoint to add question. Created new service in QuestionService class

--- src/main/java/com/test/quizapp/service/QuestionService.java
+++ src/main/java/com/test/quizapp/service/QuestionService.java
@@ -20,4 +20,10 @@ public class QuestionService {
     public List<Question> getQuestionsByCategory(String category) {
         return questionRepository.findByCategory(category);
     }
+
+
+    public String addQuestion(Question question) {
+        questionRepository.save(question);
+        return "success";
+    }
 }
